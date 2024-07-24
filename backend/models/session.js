44646475a4f8f47eb5a0650db9e7f4be0f00d091
sickbeoi <saadatullah21@gmail.com>
2024-07-25 03:34:00 +0500

    Updating the backend as whole

---
+++ backend/models/session.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        required: true
+    },
+    class: {
+        type: Schema.Types.ObjectId,
+        ref: 'Class',
+        required: true
+    },
+    teacher: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Session', sessionSchema);
