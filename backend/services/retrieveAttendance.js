02daf5bf3c4f6d438fbff0156893ae5eb0545283
sickbeoi <saadatullah21@gmail.com>
2024-12-22 06:03:55 +0500

    minor changes

--- backend/services/retrieveAttendance.js
+++ backend/services/retrieveAttendance.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAttendanceRecord } = require('../services');
+const { getAttendanceRecord } = require('../services/solanaService');
 
 // Middleware for authentication
-const { isAuth } = require('../../middlewares/authMiddleware');
+const { isAuth } = require('../../backend/middlewares/authMiddleware');
 
 // Endpoint to fetch attendance records
 router.get('/:accountPublicKey', isAuth, async (req, res) => {
