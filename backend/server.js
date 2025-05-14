ebcaceaf0a8fafe9029dbc89c6cdc6525bbd56b5
sickbeoi <saadatullah21@gmail.com>
2025-05-15 00:47:13 +0500

    chore: deployment setup

--- backend/server.js
+++ backend/server.js
@@ -40,15 +40,19 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
-.then(() => {
-    console.log('Connected to MongoDB');
-    // Start the server
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
     });
-})
-.catch(err => {
-    console.error('Failed to connect to MongoDB', err);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server Up!" })
+})
 
 module.exports = app;
